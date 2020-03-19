8c08022b74346f955344f0b2e43e9e9e0c5bf9ac
Palak Jain <palakjain@Palaks-MacBook-Pro.local>
2020-03-18 20:26:53 -0400

    Added comments and documentation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kg_palak1710_2020
+
+This program determines whether a one-to-one mapping exists between two given strings.
+
+Time complexity - O(n)
+Space complexity - O(n)
+where n is the length of the string.
+
+A boolean variable flag is used to store whether the mapping is true or false. A hashmap is used to store the one-to-one mapping of each character in the string. If a key is found, the program checks whether it's respective value matches the character in the string, otherwise the flag variable is assigned as false. If the mapping does not already exist, it adds those values to the hashmap. Finally, the flag variable is printed.
--- main.java
+++ main.java
@@ -1,3 +1,4 @@
+//Java program 
 import java.util.*;
 import java.io.*;
 
@@ -7,39 +8,44 @@ public class main {
 
         boolean flag=false;
 
+        //Getting input from command line arguments
         String s = args[0];
         String t = args[1];
 
+        //Edge case if string is null or of size 1
         if(s == null || s.length() <= 1) {
             flag=true;
             System.out.println(flag);
             return;
         }
+
+        //Used hashmap to store the one-to-one mapping of strings
         HashMap<Character, Character> map = new HashMap<Character, Character>();
+        //Iterating through the strings
         for(int i = 0 ; i< s.length(); i++){
             char a = s.charAt(i);
             char b = t.charAt(i);
+            //Checking if the mapping exists
             if(map.containsKey(a)){
-                 if(map.get(a).equals(b))
+                //To check if the value in the hashmap is same as the character in the string
+                if(map.get(a).equals(b))
                 continue;
                 else{
+                    //The mapping does not exist
                    flag=false;
                    System.out.println(flag);
                    return;
                 }
                 
             }else{
+                //Add the mapping if it does not exist
                 if(!map.containsValue(b))
                 map.put(a,b);
                 
-                
-            }
-           
+            }  
         }
     
         flag=true;
         System.out.println(flag);
-    }
-
-       
+    }   
     }
